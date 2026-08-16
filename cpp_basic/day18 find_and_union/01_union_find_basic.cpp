5f0d595ecba69ae1d307da6c6747c985b95a4d48
XJH132432-OSS <xjh132432@gmail.com>
2026-08-16 18:02:35 +0800

    day18 find and union

---
+++ cpp_basic/day18 find_and_union/01_union_find_basic.cpp
@@ -0,0 +1,96 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+
+
+
+int n=100;
+
+vector<int> ls(n,0);
+
+
+
+
+int find(int a)
+{
+    if (ls[a]==a)
+    {
+        return a;
+    }
+    else
+    {
+        return find(ls[a]);
+    }
+    
+}
+
+
+
+void isSame(int a,int b)
+{
+    if (find(a)==find(b))
+    {
+        cout<<"yes";
+    }
+    else
+    {
+        cout<<"no";
+    }
+
+}
+
+
+
+void Union(int a,int b)
+{
+    int rootx=find(a);
+
+    int rooty=find(b);
+
+    if(rootx!=rooty)
+    {
+        ls[rooty]=a;
+    }
+    
+}
+
+
+
+
+int main()
+{
+    cin>>n;
+
+    for(int i=0;i<n;i++)
+    {
+        ls[i]=i;
+    }
+
+
+
+
+    int q;
+
+    cin>>q;
+
+    while(q--)
+    {
+        int choice,num1,num2;
+
+        cin>>choice>>num1>>num2;
+
+        if (choice==1)
+        {
+            Union(num1,num2);
+        }
+        else if(choice==2)
+        {
+            isSame(num1,num2);
+        }
+
+    }
+
+
+    return 0;
+}
